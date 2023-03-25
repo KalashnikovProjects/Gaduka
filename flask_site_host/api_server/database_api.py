65338c4777a20eace18ad53b3be32560e06295cd
Kalashnikov <kalasmaks97@yandex.com>
2023-03-25 11:19:38 +0400

    Фикс API

--- flask_site_host/api_server/database_api.py
+++ flask_site_host/api_server/database_api.py
@@ -1,6 +1,4 @@
 import config
-import gaduka_engine
-
 from flask import jsonify
 from flask_restful import Resource, abort, reqparse, Api
 from flask_site_host.data import db_session
@@ -20,13 +18,20 @@ delete_parser.add_argument('token', required=True)
 
 create_project_parser = reqparse.RequestParser()
 create_project_parser.add_argument('token', required=True)
-create_project_parser.add_argument('name')
+create_project_parser.add_argument('name', required=True)
 create_project_parser.add_argument('code')
 create_project_parser.add_argument('img')
 create_project_parser.add_argument('user_id', required=True)
 
+edit_project_parser = reqparse.RequestParser()
+edit_project_parser.add_argument('token', required=True)
+edit_project_parser.add_argument('name')
+edit_project_parser.add_argument('code')
+edit_project_parser.add_argument('img')
+
 user_only = ('username', 'photo_url', 'auth_date')
 project_only = ('name', 'code', 'img', 'user.id', 'user.name')
+project_edit_only = ('name', 'code', 'img')
 
 
 def abort_if_user_not_found(user_id):
@@ -64,7 +69,6 @@ class UsersResource(Resource):
         _, user = abort_if_user_not_found(user_id)
         return jsonify({"user": user})
 
-
     def delete(self, user_id):
         args = delete_parser.parse_args()
         abort_if_token_error(args['token'])
@@ -72,10 +76,6 @@ class UsersResource(Resource):
         session.delete(user)
         session.commit()
         return jsonify({"success": "OK"})
-
-    def put(self, user_id):
-        # Пока что не нужен
-        ...
 
 
 class UsersListResource(Resource):
@@ -91,6 +91,7 @@ class UsersListResource(Resource):
         user = User(**kwa)
         session.add(user)
         session.commit()
+        session.close()
         return jsonify({"success": "OK"})
 
 
@@ -108,7 +109,14 @@ class ProjectsResource(Resource):
         return jsonify({"success": "OK"})
 
     def put(self, project_id):
-        ...
+        session, project = abort_if_project_not_found(project_id)
+        args = edit_project_parser.parse_args()
+        abort_if_token_error(args['token'])
+        for i in project_edit_only:
+            project.__dict__[i] = args[i]
+        session.commit()
+        session.close()
+        return jsonify({"success": "OK"})
 
 
 class ProjectsListResource(Resource):
