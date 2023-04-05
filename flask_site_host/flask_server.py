e2895779f8d3bf2ca4288d4e7bf2f504f765b926
Kalashnikov <kalasmaks97@yandex.com>
2023-04-05 19:42:14 +0400

    Почему не работает?

--- flask_site_host/flask_server.py
+++ flask_site_host/flask_server.py
@@ -125,7 +125,7 @@ def create_project():
     db_sess = db_session.create_session()
     project = Projects(
         name="Новый проект",
-        code='',
+        code=' ',
         user_id=current_user.id)
     db_sess.add(project)
     db_sess.commit()
