78d3ef6619f898c7dcb43db5f9623aa118fadf1a
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2023-04-24 19:04:20 +0400

    Фикс MySQL

--- flask_site_host/data/db_session.py
+++ flask_site_host/data/db_session.py
@@ -17,7 +17,7 @@ def global_init():
     conn_str = 'mysql://b38x72no83ne2fwf36qa:pscale_pw_nLy88xduCtUXA1mSjyeUdpHIVSv3pWKr2di52mkhtJA@aws.connect.psdb.cloud/gadukadatabase?ssl={"rejectUnauthorized":true}'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False, connect_args={'charset':'utf8mb4'})
+    engine = sa.create_engine(conn_str, echo=False, connect_args={'charset':'utf8mb4'}, pool_recycle=3600)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
