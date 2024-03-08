22b70b699b2548acacc4e8da8acd45e2e532b241
Maks <kalasmaks97@gmail.com>
2024-03-08 16:10:38 +0400

    Тест нового сервера MySQL

--- flask_site_host/data/db_session.py
+++ flask_site_host/data/db_session.py
@@ -1,3 +1,5 @@
+import os
+
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
@@ -13,8 +15,8 @@ def global_init():
 
     if __factory:
         return
-
-    conn_str = 'mysql://b38x72no83ne2fwf36qa:pscale_pw_nLy88xduCtUXA1mSjyeUdpHIVSv3pWKr2di52mkhtJA@aws.connect.psdb.cloud/gadukadatabase?ssl={"rejectUnauthorized":true}'
+    conn_str = os.getenv("MYSQL")
+    # 'mysql://b38x72no83ne2fwf36qa:pscale_pw_nLy88xduCtUXA1mSjyeUdpHIVSv3pWKr2di52mkhtJA@aws.connect.psdb.cloud/gadukadatabase?ssl={"rejectUnauthorized":true}'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False, connect_args={'charset':'utf8mb4'}, pool_recycle=3600)
