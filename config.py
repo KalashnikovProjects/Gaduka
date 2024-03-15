2b51b8c68e652d2b262cc0d26c25f37744a8b751
Maks <kalasmaks97@gmail.com>
2024-03-15 15:24:40 +0400

    Ой, порт не тот

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ REST_API_TOKENS = (os.environ.get('REST_API_TOKENS1'), os.environ.get('REST_API_
 MYSQL_CONNECT_STRING = os.getenv("MYSQL")
 
 DEFAULT_IMG = 'img/gaduka-icon.png'  # Изображение проекта по умолчанию
-PORT = 1000
+PORT = 10000
 TIMEOUT_RUN_GADUKA = 10
