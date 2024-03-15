85bca9c8d147f0316085bae352a030f278bf277c
Maks <kalasmaks97@gmail.com>
2024-03-15 15:30:13 +0400

    BOT_TOKEN всё таки был нужен

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 REST_API_TOKENS = (os.environ.get('REST_API_TOKENS1'), os.environ.get('REST_API_TOKENS2'))
 MYSQL_CONNECT_STRING = os.getenv("MYSQL")
 
