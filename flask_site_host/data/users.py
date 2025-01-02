193d713da03622e5d1520c90f618ffccd4128df6
KalashnikovProjects <kalasmaks97@yandex.com>
2025-01-02 20:37:19 +0400

    ORM fixes after large merge

--- flask_site_host/data/users.py
+++ flask_site_host/data/users.py
@@ -5,21 +5,20 @@ from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from sqlalchemy.dialects.mysql import BIGINT, MEDIUMTEXT, TINYTEXT
 
 
 @dataclass
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    id = sqlalchemy.Column(BIGINT,
                            primary_key=True, autoincrement=True)
-    username = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    photo_url = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    auth_date = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    username = sqlalchemy.Column(TINYTEXT, nullable=True)
+    photo_url = sqlalchemy.Column(MEDIUMTEXT, nullable=True)
+    auth_date = sqlalchemy.Column(TINYTEXT, nullable=True)
 
-    projects = orm.relationship("Projects", back_populates='user')
+    projects = orm.relationship("Projects", backref='user')
 
     def __repr__(self):
         return f"{self.username}"
--- main.py
+++
@@ -1,9 +0,0 @@
-""" Отсюда будут запускаться все остальные части проекта такие как:
-API
-Flask бекенд сайта
-Телеграмм бот
-"""
-
-from flask_site_host.flask_server import main
-
-app = main()
