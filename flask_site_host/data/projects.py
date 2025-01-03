d9d70c3b2a9e638324e332bc45ab731e26db4f00
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2025-01-04 01:40:18 +0400

    Update projects.py

--- flask_site_host/data/projects.py
+++ flask_site_host/data/projects.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 
-import config
 import sqlalchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.dialects.mysql import MEDIUMTEXT, TINYTEXT, MEDIUMINT, BIGINT
+from sqlalchemy.dialects.postgresql import TEXT, INTEGER, BIGINT
 
+from .. import config
 from .db_session import SqlAlchemyBase
 
 
@@ -12,9 +12,9 @@ from .db_session import SqlAlchemyBase
 class Projects(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'projects'
 
-    id = sqlalchemy.Column(MEDIUMINT, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(TINYTEXT, nullable=True, default="Новый проект")
-    code = sqlalchemy.Column(MEDIUMTEXT, nullable=True, default="")
-    img = sqlalchemy.Column(MEDIUMTEXT, nullable=True, default=config.DEFAULT_IMG)
+    id = sqlalchemy.Column(INTEGER, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(TEXT, nullable=True, default="Новый проект")
+    code = sqlalchemy.Column(TEXT, nullable=True, default="")
+    img = sqlalchemy.Column(TEXT, nullable=True, default=config.DEFAULT_IMG)
 
     user_id = sqlalchemy.Column(BIGINT, sqlalchemy.ForeignKey("users.id"))
