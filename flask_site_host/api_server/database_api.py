5a920b49e1d65330796de83c71887cc44c090b5e
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2025-01-03 17:22:33 +0400

    Update database_api.py

--- flask_site_host/api_server/database_api.py
+++ flask_site_host/api_server/database_api.py
@@ -1,13 +1,12 @@
 import retry
 from sqlalchemy import select
-
-import config
 from flask import jsonify
 from flask_restful import Resource, abort, reqparse
-from flask_site_host.data import db_session
-from flask_site_host.data.users import User
-from flask_site_host.data.projects import Projects
 
+from .. import config
+from ..data import db_session
+from ..data.users import User
+from ..data.projects import Projects
 
 create_user_parser = reqparse.RequestParser()
 create_user_parser.add_argument('token', required=True)
@@ -38,32 +37,29 @@ project_edit_only = ('name', 'code', 'img')
 project_create_only = ('name', 'code', 'img', "user_id")
 
 
-def abort_if_user_not_found(user_id: str):
-    with db_session.create_session() as session:
-        if user_id.isdigit():
-            user = session.get(User, user_id)
-        else:
-            user = (session.execute(
-                select(User).where(User.username == user_id)
-            )).scalar_one_or_none()
-        if not user:
-            abort(404, message=f"Пользователь {user_id} не найден")
+def abort_if_user_not_found(session, user_id: str):
+    if user_id.isdigit():
+        user = session.get(User, user_id)
+    else:
+        user = (session.execute(
+            select(User).where(User.username == user_id)
+        )).scalar_one_or_none()
+    if not user:
+        abort(404, message=f"Пользователь {user_id} не найден")
     return user
 
 
-def abort_if_project_not_found(project_id):
-    with db_session.create_session() as session:
-        project = session.get(Projects, project_id)
-        if not project:
-            abort(404, message=f"Проект {project_id} не найден")
+def abort_if_project_not_found(session, project_id):
+    project = session.get(Projects, project_id)
+    if not project:
+        abort(404, message=f"Проект {project_id} не найден")
     return project
 
 
-def abort_id_already_taken(user_id):
-    with db_session.create_session() as session:
-        user = session.get(User, user_id)
-        if user:
-            abort(404, message=f"Пользователь с id {user_id} уже существует")
+def abort_id_already_taken(session, user_id):
+    user = session.get(User, user_id)
+    if user:
+        abort(404, message=f"Пользователь с id {user_id} уже существует")
 
 
 def abort_if_token_error(token):
@@ -74,19 +70,20 @@ def abort_if_token_error(token):
 class UsersResource(Resource):
     @retry.retry(tries=3, delay=2)
     def get(self, user_id):
-        user = abort_if_user_not_found(str(user_id))
-        a = user.to_dict(only=user_only)
-        a['projects'] = []
-        for i in user.projects:
-            a['projects'].append(i.to_dict(only=("id", 'name', 'img')))
-        return jsonify({"user": a})
+        with db_session.create_session() as session:
+            user = abort_if_user_not_found(session, str(user_id))
+            a = user.to_dict(only=user_only)
+            a['projects'] = []
+            for i in user.projects:
+                a['projects'].append(i.to_dict(only=("id", 'name', 'img')))
+            return jsonify({"user": a})
 
     @retry.retry(tries=3, delay=2)
     def delete(self, user_id):
         args = delete_parser.parse_args()
         abort_if_token_error(args['token'])
         with db_session.create_session() as session:
-            user = abort_if_user_not_found(user_id)
+            user = abort_if_user_not_found(session, user_id)
             session.delete(user)
             session.commit()
         return jsonify({"success": "OK"})
@@ -97,8 +94,9 @@ class UsersListResource(Resource):
     def post(self):
         args = create_user_parser.parse_args()
         abort_if_token_error(args['token'])
-        abort_id_already_taken(args["id"])
         with db_session.create_session() as session:
+            abort_id_already_taken(session, args["id"])
+
             kwa = {}
             for i in user_only:
                 kwa[i] = args[i]
@@ -111,15 +109,16 @@ class UsersListResource(Resource):
 class ProjectsResource(Resource):
     @retry.retry(tries=3, delay=2)
     def get(self, project_id):
-        project = abort_if_project_not_found(project_id)
-        return jsonify({"project": project.to_dict(only=project_only)})
+        with db_session.create_session() as session:
+            project = abort_if_project_not_found(session, project_id)
+            return jsonify({"project": project.to_dict(only=project_only)})
 
     @retry.retry(tries=3, delay=2)
     def delete(self, project_id):
         args = delete_parser.parse_args()
         abort_if_token_error(args['token'])
         with db_session.create_session() as session:
-            project = abort_if_project_not_found(project_id)
+            project = abort_if_project_not_found(session, project_id)
             session.delete(project)
             session.commit()
         return jsonify({"success": "OK"})
@@ -129,7 +128,7 @@ class ProjectsResource(Resource):
         args = edit_project_parser.parse_args()
         abort_if_token_error(args['token'])
         with db_session.create_session() as session:
-            project = abort_if_project_not_found(project_id)
+            project = abort_if_project_not_found(session, project_id)
             for i in set(args.keys()) & set(project_edit_only):
                 setattr(project, i, args[i])
             session.commit()
