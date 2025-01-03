39b9b529a348fc31a65d07e5735ff01eb50d42fc
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2025-01-04 01:38:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,13 @@ import os
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 REST_API_TOKENS = (os.environ.get('REST_API_TOKENS1'), os.environ.get('REST_API_TOKENS2'))
-MYSQL_CONNECT_STRING = os.getenv("MYSQL")
+POSTGRES_CONNECT_STRING = os.environ.get("POSTGRES_CONNECT_STRING")
+if not POSTGRES_CONNECT_STRING:
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost:80")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
+    POSTGRES_CONNECT_STRING = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 DEFAULT_IMG = 'img/gaduka-icon.png'  # Изображение проекта по умолчанию
 PORT = 10000
