22ef0ded3fd88f43957036aeca4131a962963a6b
KalashnikovProjects <118455621+KalashnikovProjects@users.noreply.github.com>
2025-06-13 00:35:24 +0400

    Update code_page.js

--- flask_site_host/static/code_page.js
+++ flask_site_host/static/code_page.js
@@ -30,7 +30,7 @@ async function run() {
                         "images": images};
         $.ajax({
         type : "POST",
-        url : "https://gaduka.glitch.me/api/v1/engine",
+        url : "http://v2608893.hosted-by-vdsina.ru:8089/api/v1/engine",
         data: JSON.stringify(newData),
         dataType:"json",
         crossDomain: true,
